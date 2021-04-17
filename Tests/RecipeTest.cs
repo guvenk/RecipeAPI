ae70a4fc3afc65d553bc9ab07eb2f997b35be927
Guven <guven89@hotmail.com>
2021-04-17 17:46:44 +0200

    bring test into working state

--- Tests/RecipeTest.cs
+++ Tests/RecipeTest.cs
@@ -1,5 +1,9 @@
+using AutoMapper;
 using Business;
+using Business.Profiles;
 using DataAccess;
+using Moq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Tests
@@ -8,17 +12,20 @@ namespace Tests
     {
         private readonly AppDbContext _context;
         private readonly RecipeService _recipeService;
-
+        private readonly IMapper mapper;
         public RecipeTest()
         {
             _context = new AppDbContext(DbContextHelper.GetInMemoryOptions());
-            _recipeService = new RecipeService(_context,);
+            //_recipeService = new RecipeService(_context, Mock.Of<IMapper>());
+            mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile(new RecipeProfile())));
+            _recipeService = new RecipeService(_context, mapper);
         }
 
         [Fact]
-        public void Recipes_GetAll_ReturnEmptySet()
+        public async Task Recipes_GetAll_ReturnEmptySet()
         {
-
+            var result = await _recipeService.GetAllRecipes();
+            Assert.Empty(result);
         }
     }
 }
